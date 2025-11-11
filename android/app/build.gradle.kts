7c9691d4b0fd8db4bfe14450a97652658ccc9718
Koki Naniwa <namihna@icloud.com>
2025-11-11 11:05:34 +0900

    Update build configurations and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.account_template"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21  // Google Sign In requires minSdk 21
+        minSdk = flutter.minSdkVersion  // Google Sign In requires minSdk 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
